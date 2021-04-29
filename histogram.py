ee56d501815632af588990ac50220a11d68b5a1f
uranium11010 <39013436+uranium11010@users.noreply.github.com>
2021-04-29 14:56:44 -0400

    Update comment

--- histogram.py
+++ histogram.py
@@ -7,7 +7,7 @@ print("RMS:", np.sqrt(np.mean(model**2)))
 print("Q1:", np.quantile(model, 0.25), "Q2:", np.quantile(model, 0.5), "Q3:", np.quantile(model, 0.75))
 print("# val between -0.5 and 0.5:", np.count_nonzero(np.abs(model) <= 0.5), "\tthe percentage:", np.count_nonzero(np.abs(model) <= 0.5) / len(model))
 
-#generate histogram
+#generate histogram and box plot
 f, (ax_hist, ax_box) = plt.subplots(2, sharex=True, gridspec_kw={"height_ratios": (.85, .15)})
 ax_box.boxplot(model, vert=False)
 ax_hist.hist(model, bins = np.linspace(-1.6, 1, 52))
