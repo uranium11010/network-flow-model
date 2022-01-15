cd7c5c201b6c11385d4e3b5edf0bc0b67eab60eb
uranium11010 <39013436+uranium11010@users.noreply.github.com>
2022-01-15 18:30:21 -0500

    Update examples

--- example_networks.py
+++ example_networks.py
@@ -9,46 +9,40 @@ SParr = []
 sigmaarr = []
 tauarr = []
 
-#Fig. 3a
 Sarr.append(6)
 SHarr.append(9)
 SParr.append(13)
 sigmaarr.append(np.array([0, 2, 5, 6, 7, 8, SHarr[-1]]))
 tauarr.append(np.array([0, 1, 2, 5, 11, 12, 13, SParr[-1]]))
 
-#Fig. 3b
-Sarr.append(10)
+Sarr.append(11)
 SHarr.append(20)
 SParr.append(15)
-sigmaarr.append(np.array([0, 6, 9, 10, 11, 14, 15, 17, 18, 19, SHarr[-1]]))
-tauarr.append(np.array([0, 3, 4, 7, 9, 10, 11, 12, 13, 14, SParr[-1]]))
+sigmaarr.append(np.array([0, 6, 8, 9, 10, 13, 14, 16, 17, 18, 19, SHarr[-1]]))
+tauarr.append(np.array([0, 4, 5, 6, 8, 9, 10, 11, 12, 13, 14, SParr[-1]]))
 
-#Fig. 3c
 Sarr.append(5)
 SHarr.append(20)
 SParr.append(15)
-sigmaarr.append(np.array([0, 7, 13, 18, 19, SHarr[-1]]))
-tauarr.append(np.array([0, 4, 12, 13, 14, SParr[-1]]))
+sigmaarr.append(np.array([0, 13, 15, 18, 19, SHarr[-1]]))
+tauarr.append(np.array([0, 9, 12, 13, 14, SParr[-1]]))
 
-#Fig. 3d
 Sarr.append(10)
 SHarr.append(20)
 SParr.append(15)
 sigmaarr.append(np.array([0, 6, 9, 10, 11, 12, 13, 17, 18, 19, SHarr[-1]]))
 tauarr.append(np.array([0, 3, 4, 6, 7, 8, 9, 12, 13, 14, SParr[-1]]))
 
-for i in range(4):
-    S = Sarr[i]
-    SH = SHarr[i]
-    SP = SParr[i]
-    sigma = sigmaarr[i]
-    tau = tauarr[i]
+for bruh in range(4):
+    S = Sarr[bruh]
+    SH = SHarr[bruh]
+    SP = SParr[bruh]
+    sigma = sigmaarr[bruh]
+    tau = tauarr[bruh]
 
-    #energy equivalence
     d = np.array([1/SH for k in range(SH)])
     e = np.array([1/SP for k in range(SP)])
 
-    #compute model flow rates using analytic formula (Eq. 7b)
     A = np.empty((SH,), dtype=int)
     B = np.empty((SP,), dtype=int)
     for k in range(S):
@@ -62,9 +56,12 @@ for i in range(4):
             * np.prod([np.sum(d[:sigma[S-t]]) + np.sum(e[:tau[t]]) - 1 for t in range(B[y], S+1-A[x])]) \
             / np.prod([np.sum(d[:sigma[S+1-t]]) + np.sum(e[:tau[t]]) - 1 for t in range(B[y], S+2-A[x])])
 
-    #graph example model flow rates (Fig. 3a-d)
+    #current_cmap = cm.get_cmap()
+    #current_cmap.set_bad(color="black")
+    #plt.axis("off")
     fig, ax = plt.subplots()
     plt.imshow(F, origin="upper")
+    #plt.clim(0, 0.02)
     plt.colorbar()
     ax.xaxis.set_label_position("top")
     plt.xlabel("j")
@@ -75,8 +72,8 @@ for i in range(4):
     plt.xlim([0.5, SP+0.5])
     plt.ylim([SH+0.5, 0.5])
     plt.plot([0.5, SP+0.5], [SH+0.5, 0.5], c="C1")
-    if i == 0:
+    if bruh == 0:
         ax.add_patch(patches.Circle((2, 8), radius=1.7, fill=False, color="C3", lw=2))
-    elif i == 3:
+    elif bruh == 3:
         ax.add_patch(patches.Circle((9, 13), radius=2.3, fill=False, color="C3", lw=2))
     plt.show()
